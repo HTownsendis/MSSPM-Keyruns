e3a4b7780bbec2927dcc88cbfdbef0453b2548e7
Howard Townsend <1365849575113302@mil>
2021-03-23 10:21:17 -0400

    First commit of NOBA Sim analysis for MSSPM

---
+++ NOBA_sim_diet.R
@@ -0,0 +1,16 @@
+library (readr)
+setwd("C:/Users/Howard.Townsend/Documents/EM Toolbox/MSSPM Projects/NOBA_sim")
+
+
+urlfile="https://raw.githubusercontent.com/NOAA-EDAB/ms-keyrun/master/simulated-data/msinput/msspm/diet_BTS_fall_allbox_effic1.csv"
+
+NOBA_diet<-as.data.frame(read_csv(url(urlfile)))
+
+library(tidyr)
+
+library(dplyr)
+
+#Calculate average biomass of prey consumed over years
+MeanDiet <- NOBA_diet %>% group_by(species,prey) %>% summarize( MeanBiomass = mean(totdietComp))
+
+#NOBA_interactions <- cor(NOBA_sim_biomass[16:144, -1])
---
+++ NOBA_som_corr.R
@@ -0,0 +1,11 @@
+library (readr)
+setwd("C:/Users/Howard.Townsend/Documents/EM Toolbox/MSSPM Projects/NOBA_sim")
+
+
+urlfile="https://raw.githubusercontent.com/NOAA-EDAB/ms-keyrun/master/simulated-data/msinput/msspm/BTS_fall_allbox_effic1.csv"
+
+NOBA_sim_biomass<-as.data.frame(read_csv(url(urlfile)))
+
+NOBA_interactions <- cor(NOBA_sim_biomass[16:144, -1])
+
+write.csv(NOBA_interactions, "NOBA_sim_biomass_cor.csv",row.names=FALSE)
