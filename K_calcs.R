0400b59460ee0f1dbce4015d5a88eea243f3e6b2
Howard Townsend <1365849575113302@mil>
2021-04-05 16:05:35 -0400

    carrying capacity calculations

---
+++ K_calcs.R
@@ -0,0 +1,25 @@
+#################### CODE SUMMARY ##########################################
+# Description: Calculates concumption/predation coefficents and overlap 
+# coefficients for multispecies surplus production models using biomass and
+# Diet composition data as well as consumption to biomass estimates from literature.
+#
+#
+# To estimate predation coefficients for MSSPM - calculates annual 
+# consumption of prey biomass by predator biomass for a given year
+# To do later: and calculate  the mean (and Std Dev) consumption of all years 
+# to be used as initial predation coefficient.
+#
+# To estimate predation coefficients for MSSPM - uses Pianka (1973) Overlap index
+# described further below.
+#
+# Input: 1) Biomass time series and diet composition data posted to Github. 
+# Consumption to biomass estimates in a csv file.
+#
+# Output: 1) CSV files with summarized predation and overlap coefficients.
+#
+# Programmer: Howard Townsend (NOAA/NMFS/OST/ Marine Ecosystems Division) 
+# Date: March 20, 2021
+#
+# Modified: 
+# Modifications: 
+############################################################################
