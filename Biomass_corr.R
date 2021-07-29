baf669bf08ea5354a1aabd3c32de90de0adb782b
Howard Townsend <1365849575113302@mil>
2021-07-29 09:16:22 -0400

    added remove burn-in period for K calcs

--- Biomass_corr.R
+++ Biomass_corr.R
@@ -12,3 +12,8 @@ write.csv(NOBA_interactions,"Biomass_cor.csv",row.names=FALSE)
 
 write.csv(NOBA_sim_biomass, "Biomass.csv",row.names=FALSE)
 
+urlfile2="https://raw.githubusercontent.com/NOAA-EDAB/ms-keyrun/master/simulated-data/msinput/msspm/fishery_census.csv"
+
+NOBA_sim_catch<-as.data.frame(read_csv(url(urlfile2)))
+
+write.csv(NOBA_sim_catch, "Catch.csv",row.names=FALSE)
